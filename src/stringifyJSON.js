7cd5f088ce81bd7a73b0a06fcd5a469096e9814e
Ingrid Carlson <ingridclaire@gmail.com>
2020-03-16 09:39:23 -0700

    final cleanup

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -8,19 +8,26 @@ var stringifyJSON = function(obj) {
   if(obj === null) {
     return 'null';
 
-  } else if(typeof obj === 'string') {
+  } if(typeof obj === 'string') {
     return '"' + obj + '"';
 
-  } else if(typeof obj === 'number') {
+  } if(typeof obj === 'number' || typeof obj === 'boolean') {
     return obj.toString();
 
-  } else if(Array.isArray(obj)) {
+  // } if(typeof obj === 'function') {
+  //   return '';
+
+
+  } if(Array.isArray(obj)) {
       return '[' + obj.map(element => stringifyJSON(element)) + ']';
   
-  } else if(typeof obj === 'object' && !Array.isArray(obj)) {
+  } if(typeof obj === 'object' && !Array.isArray(obj)) {
     let keys = Object.keys(obj)
     let objString = ''
     for(var key in obj) {
+      if(typeof obj[key] === 'function' ) {
+        return '{}';
+      }
       if(key === keys[keys.length-1]) {
         objString += stringifyJSON(key) + ':' + stringifyJSON(obj[key])
       } else {
@@ -33,30 +40,30 @@ var stringifyJSON = function(obj) {
 
 
 
-var assertIsEqual = function(expected, actual) {
-  if(expected === actual) {
-    console.log('test passes');
-  } else {
-    console.log('Test failed: Expected ' + actual + 'to be' + expected)
-  }
-}
+// var assertIsEqual = function(expected, actual) {
+//   if(expected === actual) {
+//     console.log('test passes');
+//   } else {
+//     console.log('Test failed: Expected ' + actual + 'to be' + expected)
+//   }
+// }
 
 
 
 
-//test cases
+// //test cases
 
-var numbers = [1, [2, 3], 4, 5]
-var expected1 = JSON.stringify(numbers)
-var actual1 = stringifyJSON(numbers)
-assertIsEqual(expected1, actual1);
+// var numbers = [1, [2, 3], 4, 5]
+// var expected1 = JSON.stringify(numbers)
+// var actual1 = stringifyJSON(numbers)
+// assertIsEqual(expected1, actual1);
 
-var animals = ['Luna', 'George', 'Severus']
-var expected2 = JSON.stringify(animals)
-var actual2 = stringifyJSON(animals)
-assertIsEqual(expected2, actual2);
+// var animals = ['Luna', 'George', 'Severus']
+// var expected2 = JSON.stringify(animals)
+// var actual2 = stringifyJSON(animals)
+// assertIsEqual(expected2, actual2);
 
-var songs = {taylorSwift: "Lover", beyonce: {lemonadeAlbum: ["Pray You Catch Me", "Sorry", "Sandcastles"]}, brandiCarlile: 'The Joke'};
-var expected3 = JSON.stringify(songs)
-var actual3 = stringifyJSON(songs)
-assertIsEqual(expected3, actual3);
+// var songs = {taylorSwift: "Lover", beyonce: {lemonadeAlbum: ["Pray You Catch Me", "Sorry", "Sandcastles"]}, brandiCarlile: 'The Joke'};
+// var expected3 = JSON.stringify(songs)
+// var actual3 = stringifyJSON(songs)
+// assertIsEqual(expected3, actual3);
