3a33146e5b0da145f675c42bf5bf7938c77d9db2
Ingrid Carlson <ingridclaire@gmail.com>
2020-03-16 17:11:34 -0700

    first pass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var elementChildren = Array.prototype.slice.call(this.childNodes)
+  var childrenWithClass = elementChildren.filter(child => {
+    child.classList.contains(className)
+  })
+
+  var classArray = Array.prototype.slice.call(elementChildren.classList)
+
+  if(!this.hasChildNodes()) {
+    return [];
+
+  } else {
+      return childrenWithClass.concat(elementChildren.map(child => {
+        child.getElementsByClassName(className);
+      }));
+  }
+
 };
